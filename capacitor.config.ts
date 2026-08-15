49d3d6064bde48f8e23012a3d6fead5dd287317e
satishmehtre037 <satishmehtre037@gmail.com>
2026-08-16 01:29:25 +0530

    chore: connect Capacitor mobile app to live Vercel production URL

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,8 +5,8 @@ const config: CapacitorConfig = {
   appName: 'BizBot OS',
   webDir: 'out',
   server: {
-    url: 'http://192.168.0.112:3000',
-    cleartext: true,
+    url: 'https://orderagentapp.vercel.app',
+    cleartext: false,
   },
   android: {
     allowMixedContent: true,
